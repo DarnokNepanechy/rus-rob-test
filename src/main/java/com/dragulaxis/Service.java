1980ce71598bc5a582e38fd188b18231abb95089
Darnok Nepanechy <dushesgt@gmail.com>
2022-01-19 07:56:55 +0300

    add service

--- src/main/java/com/dragulaxis/Service.java
+++ src/main/java/com/dragulaxis/Service.java
@@ -2,17 +2,34 @@ package com.dragulaxis;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Service {
+    private static final String PATH_TO_DB = "src\\main\\resources\\db\\sqlite\\sqliteDB.db";
+
     public static void main(String[] args) {
 
+        String sql = "CREATE TABLE IF NOT EXISTS test_table (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	name text NOT NULL,\n"
+                + "	capacity real\n"
+                + ");";
+
+        try (
+                Connection connection = DriverManager.getConnection(PATH_TO_DB);
+                Statement statement = connection.createStatement()
+        ) {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    // src\main\resources\db\sqlite\sqliteDB.db
-    public static void createDb(String pathAndFileName) {
+    public static void createDb(String path) {
         try {
             Class.forName("org.sqlite.JDBC");
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:" + pathAndFileName);
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:" + path);
             connection.close();
         } catch (Exception e) {
             e.printStackTrace();
