7abfbef3694c1a47e6da32363799bb20154417ac
Darnok Nepanechy <dushesgt@gmail.com>
2022-01-21 03:07:26 +0300

    added parameter setting

--- src/main/java/ru/dragulaxis/Application.java
+++ src/main/java/ru/dragulaxis/Application.java
@@ -1,43 +1,70 @@
 package ru.dragulaxis;
 
-import com.dragulaxis.PriceItem;
 import com.opencsv.CSVParser;
 import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
-import com.opencsv.exceptions.CsvException;
+import com.opencsv.exceptions.CsvValidationException;
 
 import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Application {
-    static List<PriceItem> priceItemList = new ArrayList<>();
+    static int column1 = 1;
+    static int column2 = 10;
+    static int column5 = 3;
+    static int column6 = 6;
+    static int column7 = 8;
 
-    public static void main(String[] args) throws IOException, CsvException, SQLException {
+    static String mailHost = "imap.yandex.ru";
+    static int mailPort = 993;
+    static String mailLogin = "test17012022@yandex.ru";
+    static String mailPassword = "qyuyjrnkurpzrhrx";
 
-        // скачивание файла с моей почты
-        MailClient client = new MailClient("imap.yandex.ru", 993, "test17012022@yandex.ru", "qyuyjrnkurpzrhrx");
-        String filePath = client.downloadCsvFile("Hello World");
+    static String from = "Hello World";
 
-        // TODO: поменять на filePath
-        File file = new File("C:\\temporary\\file15672310898083658598.csv");
+    public static void main(String[] args) throws IOException, SQLException {
+
+        String choice = "";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (!choice.equals("4")){
+            info();
+            choice = br.readLine();
+            switch (choice) {
+                case ("1") -> tableSetting(br);
+                case ("2") -> mailSetting(br);
+                case ("3") -> {
+                    System.out.print("Присылатель: ");
+                    from = br.readLine();
+                }
+                case ("4") -> br.close();
+                case ("5") -> {
+                    br.close();
+                    return;
+                }
+            }
+        }
+
+        MailClient client = new MailClient(mailHost, mailPort, mailLogin, mailPassword);
+        String filePath = client.downloadCsvFile(from);
+
+        File file = new File(filePath);
 
         CSVParser parser = new CSVParserBuilder()
                 .withSeparator(';')
                 .build();
+
         CSVReader reader = new CSVReaderBuilder(new FileReader(file))
                 .withSkipLines(1)
                 .withCSVParser(parser)
                 .build();
 
         String PATH_TO_DB = "src\\main\\resources\\db\\sqliteDB.db";
-        Repository repository = new Repository();
-        repository.createDb();
+        Repository repository = new Repository(column1, column2, column5, column6, column7);
         repository.createTable();
 
         Connection connection = DriverManager.getConnection("jdbc:sqlite:" + PATH_TO_DB);
@@ -47,16 +74,75 @@ public class Application {
         int i = 1;
         try {
             String[] nextLine;
+            statement.execute("DELETE FROM PriceItems;");
+            System.out.println("Загрузка данных в базу...");
             while ((nextLine = reader.readNext()) != null) {
                 statement.execute(repository.createSQLRequest(nextLine));
                 i++;
             }
-        } catch (Exception e) {
+        } catch (CsvValidationException e) {
+            System.out.println("Не удалось обработать строку: " + i);
             e.printStackTrace();
         } finally {
             connection.commit();
-            System.out.println("Не удалось обработать строку: " + i);
+            connection.close();
+            statement.close();
+            System.out.println("Добавлено " + (i - 1) + " полей");
         }
         connection.commit();
+    }
+
+    static void info() {
+        System.out.println("Текущие настройки прайс-листа (нумерация с 0):");
+        System.out.println("Номер колонки \"Бренд\": " + column1);
+        System.out.println("Номер колонки \"Каталожный номер\": " + column2);
+        System.out.println("Номер колонки \"Описание\": " + column5);
+        System.out.println("Номер колонки \"Цена, руб.\": " + column6);
+        System.out.println("Номер колонки \"Наличие\": " + column7);
+        System.out.println();
+        System.out.println("Текущие настройки почты:");
+        System.out.println("Хост: " + mailHost);
+        System.out.println("Порт: " + mailPort);;
+        System.out.println("Логин: " + mailLogin);
+        System.out.println();
+        System.out.println("Присылает почту: " + from);
+        System.out.println();
+        System.out.println("Введите соответствующую цифру для команды:");
+        System.out.println("1. Настройка прайс-листа поставщика. ");
+        System.out.println("2. Настройка почты. ");
+        System.out.println("3. Сменить присылателя. ");
+        System.out.println("4. Начать выполнение с текущими настройками! ");
+        System.out.println("5. Выйти из программы. ");
+        System.out.println();
+    }
+
+    static void tableSetting(BufferedReader br) throws IOException {
+        System.out.println();
+        System.out.println("Настройка прайс-листа поставщика: ");
+        System.out.print("Номер колонки \"Бренд\": ");
+        column1 = Integer.parseInt(br.readLine());
+        System.out.print("Номер колонки \"Каталожный номер\": ");
+        column2 = Integer.parseInt(br.readLine());
+        System.out.print("Номер колонки \"Описание\": ");
+        column5 = Integer.parseInt(br.readLine());
+        System.out.print("Номер колонки \"Цена, руб.\": ");
+        column6 = Integer.parseInt(br.readLine());
+        System.out.print("Номер колонки \"Наличие\": ");
+        column7 = Integer.parseInt(br.readLine());
+        System.out.println();
+    }
+
+    static void mailSetting(BufferedReader br) throws IOException {
+        System.out.println();
+        System.out.println("Настройка почты: ");
+        System.out.print("Хост: ");
+        mailHost = br.readLine();
+        System.out.print("Порт: ");
+        mailPort = Integer.parseInt(br.readLine());
+        System.out.print("Логин: ");
+        mailLogin = br.readLine();
+        System.out.print("Пароль: ");
+        mailPassword = br.readLine();
+        System.out.println();
     }
 }
--- src/main/java/ru/dragulaxis/Repository.java
+++ src/main/java/ru/dragulaxis/Repository.java
@@ -4,16 +4,28 @@ import java.sql.*;
 import java.util.Locale;
 
 public class Repository {
-    String PATH_TO_DB = "src\\main\\resources\\db\\sqliteDB.db";
+    private final int column1;
+    private final int column2;
+    private final int column5;
+    private final int column6;
+    private final int column7;
+
+    public Repository(int column1, int column2, int column5, int column6, int column7) {
+        this.column1 = column1;
+        this.column2 = column2;
+        this.column5 = column5;
+        this.column6 = column6;
+        this.column7 = column7;
+    }
 
     public String createSQLRequest(String[] fields) {
-        String vendor = fields[1].toUpperCase();
-        String number = fields[10].toUpperCase();
-        String searchVendor = fields[1].replaceAll("[^A-Za-z0-9]", "").toUpperCase(Locale.ROOT);
-        String searchNumber = fields[10].replaceAll("[^A-Za-z0-9]", "").toUpperCase(Locale.ROOT);
-        String description = fields[3].length() > 512 ? fields[3].substring(0, 512) : fields[3];
-        Double price = Double.parseDouble(fields[6].replace(",", "."));
-        int count = Integer.parseInt(fields[8].replaceAll("[^0-9]", ""));
+        String vendor = fields[column1];
+        String number = fields[column2];
+        String searchVendor = fields[column1].replaceAll("[^A-Za-z0-9]", "").toUpperCase(Locale.ROOT);
+        String searchNumber = fields[column2].replaceAll("[^A-Za-z0-9]", "").toUpperCase(Locale.ROOT);
+        String description = fields[column5].length() > 512 ? fields[column5].substring(0, 512) : fields[column5];
+        Double price = Double.parseDouble(fields[column6].replace(",", "."));
+        int count = Integer.parseInt(fields[column7].replaceAll("[^0-9]", ""));
 
         return String.format(
                 "INSERT INTO PriceItems " +
@@ -26,7 +38,6 @@ public class Repository {
 
     public void createTable() {
         String sql = "CREATE TABLE IF NOT EXISTS PriceItems (\n"
-//                + "id integer PRIMARY KEY, \n" // возможно, это поле заменит number
                 + "Vendor varchar(64), \n"
                 + "Number varchar(64), \n"
                 + "SearchVendor varchar(64), \n"
@@ -36,20 +47,11 @@ public class Repository {
                 + "Count int \n"
                 + ");";
         try (
-                Connection connection = DriverManager.getConnection("jdbc:sqlite:" + PATH_TO_DB);
+                Connection connection = DriverManager.getConnection("jdbc:sqlite:" + "src\\main\\resources\\db\\sqliteDB.db");
                 Statement statement = connection.createStatement()
         ) {
             statement.execute(sql);
         } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void createDb() {
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:" + PATH_TO_DB);
-            connection.close();
-        } catch (Exception e) {
             e.printStackTrace();
         }
     }
